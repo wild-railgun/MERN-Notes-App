820299fc06121eb7d7758490335055569dc15941
wild-railgun <wildrailgun@gmail.com>
2024-01-24 00:35:42 +0500

    added login, and signup pages and some other changes

--- frontend2/src/App.js
+++ frontend2/src/App.js
@@ -1,20 +1,32 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext'
 
 // pages & components
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
+import Signup from './pages/Signup'
+import Navbar from './components/Navbar'
 
 function App() {
+  const { user } = useAuthContext()
 
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className="pages">
-          <Routes>
+        <Routes>
             <Route 
               path="/" 
-              element={<Home />} 
+              element={user ? <Home /> : <Navigate to="/login" />} 
+            />
+            <Route 
+              path="/login" 
+              element={!user ? <Login /> : <Navigate to="/" />} 
+            />
+            <Route 
+              path="/signup" 
+              element={!user ? <Signup /> : <Navigate to="/" />} 
             />
           </Routes>
         </div>
--- frontend2/src/index.js
+++ frontend2/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { NotesContextProvider } from './context/NotesContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NotesContextProvider>
-      <App />
-    </NotesContextProvider>
+    <AuthContextProvider>
+      <NotesContextProvider>
+        <App />
+      </NotesContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 )
