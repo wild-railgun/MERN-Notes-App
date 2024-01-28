1649caa45c7fa5912db0609f9068ff04ed7b8b17
wild-railgun <wildrailgun@gmail.com>
2024-01-29 00:57:13 +0500

    added some things in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,53 +1,46 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const mongoose = require('mongoose')
-const notesRoutes = require('./routes/notes')
-const userRoutes = require('./routes/user')
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const notesRoutes = require('./routes/notes');
+const userRoutes = require('./routes/user');
 
 // express app
-const app = express()
-// app.use(cors(
-//   {
-//     origin: ["https://deploy-mern-lwhq.vercel.app"],
-//     methods: ["POST", "GET"],
-//     credentials: true
-//   }
-// ));
+const app = express();
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 
-const __dirname = path.resolve();
-app.use('/uploads', express.static('/var/data/uploads'));
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-
-app.get('*', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-);
-
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to my notes app'})
-})
-
+// Logging Middleware
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 // routes
-app.use('/api/notes', notesRoutes)
-app.use('/api/user', userRoutes)
+app.use('/api/notes', notesRoutes);
+app.use('/api/user', userRoutes);
+
+// Static file serving
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/var/data/uploads')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+// Serve React App
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+});
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('connected to database')
+    console.log('Connected to database');
     // listen to port
     app.listen(process.env.PORT, () => {
-      console.log('listening for requests on port', process.env.PORT)
-    })
+      console.log('Listening for requests on port', process.env.PORT);
+    });
   })
   .catch((err) => {
-    console.log(err)
-  }) 
+    console.log(err);
+  });
